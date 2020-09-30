0ddbed9a4d6b58c6f2d03c00beeeebb18f8e95ab
Timothy Graupmann <tim.graupmann@razer.com>
2020-09-30 08:07:44 -0700

    Set URL on startup

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Security.Authentication.Web;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -76,7 +66,12 @@ namespace UWP_WAB
 
         private async void BtnOpen_Click(object sender, RoutedEventArgs e)
         {
-            await OpenWAB(_mTextUrl.Text);
+            await OpenWAB(_mTxtUrl.Text);
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            _mTxtUrl.Text = string.Format("https://sample.com/oath2?guid={0}", Guid.NewGuid());
         }
     }
 }
